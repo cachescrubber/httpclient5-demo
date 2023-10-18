21696ee4987dfd1254977768becc4dfe78c3b47e
Lars Uffmann <lars.uffmann@gmail.com>
2023-10-18 14:01:51 +0200

    Add basicAuthCanBeEnabledWhenBuffering

--- src/test/java/com/example/httpclient5demo/Httpclient5DemoApplicationTests.java
+++ src/test/java/com/example/httpclient5demo/Httpclient5DemoApplicationTests.java
@@ -2,19 +2,57 @@ package com.example.httpclient5demo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.apache.hc.client5.http.auth.AuthScope;
+import org.apache.hc.client5.http.auth.UsernamePasswordCredentials;
 import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.auth.BasicCredentialsProvider;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConfigurer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class Httpclient5DemoApplicationTests {
+
+	@Test
+	void basicAuthCanBeEnabled() {
+		RestTemplate restTemplate = new RestTemplate();
+		BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+
+		credentialsProvider.setCredentials(new AuthScope("httpbin.org", 443),
+				new UsernamePasswordCredentials("demo", "secret".toCharArray()));
+
+		HttpClientBuilder httpClientBuilder = HttpClientBuilder.create()
+				.setDefaultCredentialsProvider(credentialsProvider);
+
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClientBuilder.build());
+		restTemplate.setRequestFactory(requestFactory);
+		ResponseEntity<String> forEntity = restTemplate.getForEntity("https://httpbin.org//basic-auth/demo/secret", String.class);
+		assertThat(forEntity.getStatusCode().is2xxSuccessful()).isTrue();
+	}
+
+	@Test
+	void basicAuthCanBeEnabledWhenBuffering() {
+		RestTemplate restTemplate = new RestTemplate();
+		BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+
+		credentialsProvider.setCredentials(new AuthScope("httpbin.org", 443),
+				new UsernamePasswordCredentials("demo", "secret".toCharArray()));
+
+		HttpClientBuilder httpClientBuilder = HttpClientBuilder.create()
+				.setDefaultCredentialsProvider(credentialsProvider);
+
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClientBuilder.build());
+		restTemplate.setRequestFactory(new BufferingClientHttpRequestFactory(requestFactory));
+		ResponseEntity<String> forEntity = restTemplate.getForEntity("https://httpbin.org//basic-auth/demo/secret", String.class);
+		assertThat(forEntity.getStatusCode().is2xxSuccessful()).isTrue();
+	}
 
 	@Test
 	void redirectsCanBeEnabled() {
